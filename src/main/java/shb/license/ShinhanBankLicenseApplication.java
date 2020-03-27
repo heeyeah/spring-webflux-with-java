8ce5a79c9127337f025aaca1b95d23598cb79169
heeyeah <gdshy221@gmail.com>
2020-03-27 17:49:35 +0900

    init spring boot with java (maven, webflux..)

---
+++ src/main/java/shb/license/ShinhanBankLicenseApplication.java
@@ -0,0 +1,13 @@
+package shb.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanBankLicenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinhanBankLicenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shb/license/ShinhanBankLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package shb.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhanBankLicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
