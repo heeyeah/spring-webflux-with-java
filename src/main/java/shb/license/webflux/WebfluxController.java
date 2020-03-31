1c4dde34b1ff04ec2211351eaf5172f63527e3c8
heeyeah <gdshy221@gmail.com>
2020-03-31 18:07:13 +0900

    write test script

---
+++ src/main/java/shb/license/webflux/WebfluxController.java
@@ -0,0 +1,13 @@
+package shb.license.webflux;
+
+import org.springframework.context.annotation.Description;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Description("어노테이션 기반 라우팅")
+public class WebfluxController {
+
+
+
+
+}
---
+++ src/test/java/shb/license/reactor/ReactorModuleTest.java
@@ -0,0 +1,64 @@
+package shb.license.reactor;
+
+import org.junit.jupiter.api.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ReactorModuleTest {
+
+    @Test
+    @DisplayName("Refactor Mono Test")
+    public void monoTest() {
+
+        List<String> emptyList = new ArrayList<>();
+
+        Mono<String> noData = Mono.empty();
+        noData.subscribe(emptyList::add);
+        System.out.println("noData length : " + emptyList.size());
+
+        assertEquals(0, emptyList.size());
+
+        Mono<String> data = Mono.just("Cloud");
+        data.log().subscribe(emptyList::add);
+        System.out.println("data length : " + emptyList.size());
+        System.out.println("data value : " + ((emptyList.size() > 0) ? emptyList.get(0) : ""));
+
+        assertEquals(1, emptyList.size());
+    }
+
+    @Test
+    @DisplayName("Refactor Flux Test")
+    public void fluxTest() {
+
+
+        Flux<String> seq1 = Flux.just("hello", "world", ":)");
+
+        List<String> iterable = Arrays.asList("foo", "bar", "foobar");
+        Flux<String> seq2 = Flux.fromIterable(iterable);
+
+        seq1.subscribe(); // Subscribe and trigger the sequence.
+        seq2.subscribe(i -> System.out.println(i)); //	Do something with each produced value.
+
+
+        Flux<Integer> ints = Flux.range(1, 4)
+                .map(i -> {
+                    if (i <= 3) return i;
+                    throw new RuntimeException("Got to 4");
+                });
+        ints.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error: " + error)); //Deal with values but also react to an error.
+
+        Flux<Integer> ints2 = Flux.range(1, 4);
+        ints2.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> System.out.println("Done")); //	Deal with values and errors but also run some code when the sequence successfully completes.
+
+    }
+}
