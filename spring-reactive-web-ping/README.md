ff894f43454700187a50edd6746e1dd17f3a875b
heeyeah <gdshy221@gmail.com>
2020-04-07 20:51:25 +0900

    init spring-reactive-web-ping project for performance test

---
+++ spring-reactive-web-ping/README.md
@@ -0,0 +1,6 @@
+
+### spring reactive web 
+dependency : spring-boot-starter-webflux
+
+### ping url
+http://localhost:9000/webflux/ping
---
+++ spring-reactive-web-ping/src/main/java/com/webflux/SpringReactiveWebPingApplication.java
@@ -0,0 +1,13 @@
+package com.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWebPingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveWebPingApplication.class, args);
+	}
+
+}
